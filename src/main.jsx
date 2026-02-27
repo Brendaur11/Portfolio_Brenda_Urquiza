ac82e5697031b156f43e362b4131846e8de52a95
Brenda Urquiza <burquiza@jla.local>
2026-02-27 15:44:38 -0300

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Portfolio_Brenda_Urquiza'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
